0649a92381c4b19ffa5a201cffa23209b08027a2
J2b2590 <J2b2590@gmail.com>
2020-09-17 20:40:12 -0500

    finish lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require "pry"
+
+class School
+
+    attr_accessor :roster
+    
+
+    def initialize(roster)
+
+        @roster = {}
+        
+
+    end
+
+    def add_student(student, grade)
+        
+        roster[grade] ||= []
+
+
+        roster[grade] << student
+        
+    end
+
+    def grade(integer)
+        
+        roster[integer]
+    end
+
+    def sort
+        roster.each_value do |i|
+            i.sort!
+        end
+    end
+
+end
